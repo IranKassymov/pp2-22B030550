f4133b52c7557648a2813980e53e5b9c2f335480
IranKassymov <rick_shuh@mail.ru>
2023-03-30 18:18:33 +0600

    Update ex1.py

--- tsis7/ex1.py
+++ tsis7/ex1.py
@@ -2,34 +2,36 @@ import pygame
 import datetime
 pygame.init()
 
-width, height = 800, 600
+width, height = 800, 450
 screen = pygame.display.set_mode((width, height))
 screen.fill((255, 255, 255))
 
 clock = pygame.image.load("image/mickey.jpeg")
-scale_clock = pygame.transform.scale(clock, (clock.get_width() // 2, clock.get_height() // 2))
-clockr = scale_clock.get_rect(center=(width/2, height/2))
+clock_scale = pygame.transform.scale(clock, (clock.get_width() // 2, clock.get_height() // 2))
+clock_rect = clock_scale.get_rect(center=(width//2, height//2))
 
 second = pygame.image.load("image/arrow.png")
-scale_second = pygame.transform.scale(second, (second.get_width() // 2, second.get_height() // 2))
+second_scale = pygame.transform.scale(second, (second.get_width() // 2, second.get_height() // 2))
 
 minute = pygame.image.load("image/arrow.png")
-scale_minute = pygame.transform.scale(minute, (minute.get_width() // 2, minute.get_height() // 2))
+minute_scale = pygame.transform.scale(minute, (minute.get_width() // 2, minute.get_height() // 2))
 
-running = True
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
 
     screen.fill((255, 255, 255))
-    screen.blit(scale_clock, clockr)
+    screen.blit(clock_scale, clock_rect)
     t = datetime.datetime.now()
     minutes, seconds = t.minute, t.second
-    rots = pygame.transform.rotate(scale_second, (-6*seconds)+90)
-    rots_rect = rots.get_rect(center=(400, 300))
-    screen.blit(rots, rots_rect)
-    rotm = pygame.transform.rotate(scale_minute, (-6*minutes)+90)
-    rotm_rect = rotm.get_rect(center=(400, 300))
-    screen.blit(rotm, rotm_rect)
+
+    second_rotation = pygame.transform.rotate(second_scale, (seconds * -6) + 90)
+    second_rect = second_rotation.get_rect(center=(width // 2, height // 2))
+    screen.blit(second_rotation, second_rect)
+
+    minute_rotation = pygame.transform.rotate(minute_scale, (minutes * -6) + 90)
+    minute_rect = minute_rotation.get_rect(center=(width // 2, height // 2))
+    screen.blit(minute_rotation, minute_rect)
+
     pygame.display.update()
