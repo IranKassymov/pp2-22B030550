324553f794ad59c5726f606b9c571255dec70a7d
IranKassymov <rick_shuh@mail.ru>
2023-04-03 23:13:40 +0600

    Update snake.py

--- tsis8/snake/snake.py
+++ tsis8/snake/snake.py
@@ -20,34 +20,31 @@ clock = pygame.time.Clock()
 font_score = pygame.font.SysFont('Arial', 26, bold=True)
 font_end = pygame.font.SysFont('Arial', 66, bold=True)
 
+
 def close_game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
 
+
 while True:
     surface.fill((0, 0, 0))
-    # drawing snake, apple
     [pygame.draw.rect(surface, pygame.Color('green'), (i, j, SIZE - 1, SIZE - 1)) for i, j in snake]
     pygame.draw.rect(surface, pygame.Color('red'), (*apple, SIZE, SIZE))
-    # show score
     render_score = font_score.render(f'SCORE: {score}', 1, pygame.Color('orange'))
     surface.blit(render_score, (5, 5))
-    # snake movement
     speed_count += 1
     if not speed_count % snake_speed:
-	    x += dx * SIZE
-	    y += dy * SIZE
-	    snake.append((x, y))
-	    snake = snake[-length:]
-    # eating food
+        x += dx * SIZE
+        y += dy * SIZE
+        snake.append((x, y))
+        snake = snake[-length:]
     if snake[-1] == apple:
         apple = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
         length += 1
         score += 1
         snake_speed -= 1
         snake_speed = max(snake_speed, 4)
-    # game over
     if x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE or len(snake) != len(set(snake)):
         while True:
             render_end = font_end.render('GAME OVER', 1, pygame.Color('orange'))
@@ -58,7 +55,6 @@ while True:
     pygame.display.flip()
     clock.tick(fps)
     close_game()
-    # controls
     key = pygame.key.get_pressed()
     if key[pygame.K_w]:
         if dirs['W']:
